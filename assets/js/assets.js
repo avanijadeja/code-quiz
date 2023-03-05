c8a6e114606b24a6546a85bcf5b3038c19962ed5
Avani <avani.jadeja@gmail.com>
2023-03-05 16:29:53 -0600

    Add questions and answers in assets.js file

---
+++ assets/js/assets.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        title: "Which event occurs when the user clicks on an HTML element?",
+        choices: ["onchange", "onclick", "onmouseover", "onmouseclick"],
+        answer: "onclick"
+    },
+    {
+        title: "How do you create a function in JavaScript?",
+        choices: ["function:myFunction()", "function = myFunction()", "function myFunction()", "function myFunction"],
+        answer: "function myFunction()"
+    },
+    {
+        title: "How to write an IF statement in JavaScript?",
+        choices: ["if i == 5 then", "if (i == 5)", "if i = 5", "if i = 5 then"],
+        answer: "if (i == 5)"
+    },
+    {
+        title: "How do you round the number 7.25, to the nearest integer?",
+        choices: ["round(7.25)", "Math.rnd(7.25)", "Math.round(7.25)", "rnd(7.25)"],
+        answer: "Math.round(7.25)"
+    },
+    {
+        title: "How do you find the number with the highest value of x and y",
+        choices: ["Math.max(x, y)", "ceil(x, y)", "top(x, y)", "Math.ceil(x, y)"],
+        answer: "Math.max(x, y)"
+    },
+
+];
