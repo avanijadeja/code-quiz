622e76459234550619698ea9939a9ff81876681c
Avani <avani.jadeja@gmail.com>
2023-03-05 19:39:59 -0600

    set initial varibles and value in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',(event) => {
+
+const initialTime =75;
+let time =75;
+let score = 0;
+let qCount =0;
+let timeset;
+let answers = document.querySelectorAll('quizHolder button');
+
+
+});
